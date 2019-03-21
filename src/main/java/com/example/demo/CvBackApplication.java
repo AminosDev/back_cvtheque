1d4b94f4b5c8194d18bbb7698f9224b8402c3caa
AminosDev <48357980+AminosDev@users.noreply.github.com>
2019-03-21 09:25:18 +0100

    delete warning

--- src/main/java/com/example/demo/CvBackApplication.java
+++ src/main/java/com/example/demo/CvBackApplication.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
 
-
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
--- src/main/java/com/example/demo/models/Utilisateur.java
+++ src/main/java/com/example/demo/models/Utilisateur.java
@@ -1,13 +1,13 @@
 package com.example.demo.models;
 
-import javax.persistence.CascadeType;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+
 
 @Entity
 public class Utilisateur {
